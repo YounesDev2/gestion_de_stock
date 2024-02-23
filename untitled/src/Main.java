115a1c8982230509325eb8fe28b6dce0968200c9
YounesDev2 <youneslarfaoui2@gmail.com>
2024-02-23 11:48:01 +0100

    OK

---
+++ untitled/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(30 * 40);
+        System.out.println(30 * 40);
+        System.out.println(30 * 40);
+        System.out.println(30 * 40);
+        System.out.println(30 * 40);
+        System.out.println(30 * 40);
+        System.out.println(30 * 40);
+        System.out.println(30 * 40);
+        System.out.println(30 * 40);
+        System.out.println(30 * 40);
+        System.out.println(30 * 40);
+        System.out.println(30 * 40);
+    }
+}
